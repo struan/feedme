f1ec8375569c2e78dc92cfefa73167967662916f
Struan Donald <struan@exo.org.uk>
2012-10-18 23:07:15 +0100

    quick hack in of item list display

--- lib/feedme.pm
+++ lib/feedme.pm
@@ -1,10 +1,14 @@
 package feedme;
 use Dancer ':syntax';
+use Dancer::Plugin::DBIC 'schema';
 
 our $VERSION = '0.1';
 
 get '/' => sub {
-    template 'index';
+    my $items = [ schema->resultset('Item')->search(undef, { join => 'id_feed' } )->all ];
+    template 'index' => {
+        items => $items
+    };
 };
 
 true;
