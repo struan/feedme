16772905891bac47cdfb18f3eba587f2ccf46bcf
Struan Donald <struan@exo.org.uk>
2012-11-07 00:10:40 +0000

    show newest things first

--- lib/feedme.pm
+++ lib/feedme.pm
@@ -5,7 +5,7 @@ use Dancer::Plugin::DBIC 'schema';
 our $VERSION = '0.1';
 
 get '/' => sub {
-    my $items = [ schema->resultset('Item')->search(undef, { join => 'feed' } )->all ];
+    my $items = [ schema->resultset('Item')->search(undef, { order_by => { -desc => 'last_update' }, join => 'feed' } )->all ];
     template 'index' => {
         items => $items
     };
