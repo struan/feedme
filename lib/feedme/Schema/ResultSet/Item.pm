572ca3b013590c50a98465ee286800a563667015
Struan Donald <struan@exo.org.uk>
2015-01-01 18:26:20 +0000

    very rudimentary paged archive support
    only semi-functional at the moment but just about a start

--- lib/feedme/Schema/ResultSet/Item.pm
+++ lib/feedme/Schema/ResultSet/Item.pm
@@ -5,17 +5,40 @@ sub get_unread {
     my $self = shift;
     my $feed = shift;
 
-    my $params = { viewed => 0 };
+    return $self->_get_items( 0, 0, $feed );
+}
+
+sub get_read {
+    my $self = shift;
+    my $feed = shift;
+    my $page = shift;
+
+    return $self->_get_items( 1, $page, 30, $feed );
+}
+
+sub _get_items {
+    my $self = shift;
+    my $viewed = shift;
+    my $page = shift;
+    my $paged = shift;
+    my $feed = shift;
+
+    my $params = { viewed => $viewed  };
     $params->{feed_id} = $feed if $feed;
+
+    my $options = {
+        order_by => { -desc => 'last_update' },
+        join     => 'feed',
+    };
+    if ( $paged ) {
+        $options->{page} = $page || 1;
+        $options->{rows} = $paged;
+    }
 
     return $self->search(
         $params,
-        {
-            order_by => { -desc => 'last_update' },
-            join     => 'feed',
-        }
+        $options
     );
 }
-
 
 1;
