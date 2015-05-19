655ab44249bc6659bbb68f4f610bbe51392ba9b6
Struan Donald <struan@exo.org.uk>
2015-05-19 22:52:47 +0100

    prevent uninitialized value warning

--- lib/feedme/Parse.pm
+++ lib/feedme/Parse.pm
@@ -157,8 +157,8 @@ sub parse_rss {
             # sometime we need to use the base URI in combination 
             # with the link to get an absolute URI...
             if ( not $link or $link !~ /^https?:/ ) {
-                Dancer::debug "DEBUG: still no http in link or id, useing _uri_to_abs on
-                      $link and " . $parser->link . "\n";
+                Dancer::debug "DEBUG: still no http in link or id, useing _uri_to_abs on "
+                      . ( $link || 'missing link' ) . " and " . $parser->link . "\n";
                 my $base = $parser->link;
                 $base = $args{feed_uri} if $base eq '.' or $base !~ /^https?:/;
                 $link = feedme::Util::uri_to_abs($link, $base);    
